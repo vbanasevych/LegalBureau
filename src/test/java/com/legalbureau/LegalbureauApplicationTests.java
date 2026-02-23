255b9b1cc9e3c942cccd30aad51f0ca923653bec
vbanasevych <viktoriabanasevic@gmail.com>
2026-02-23 22:42:26 +0200

    project set up

---
+++ src/test/java/com/legalbureau/LegalbureauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legalbureau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalbureauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
